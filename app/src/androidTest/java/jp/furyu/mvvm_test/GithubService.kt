7acfa3d6eaed447853dbdbad8dd28cd4da0340e1
kentrino <tachikoma@h-kento.jp>
2018-08-02 14:40:13 +0900

    add GithubService

---
+++ app/src/androidTest/java/jp/furyu/mvvm_test/GithubService.kt
@@ -0,0 +1,17 @@
+package jp.furyu.mvvm_test
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GithubService {
+    @GET("users/{user}/repos")
+    fun getProjectList(@Path("user") user: String): Call<List<Project>>
+
+    @GET("/repos/{user}/{reponame}")
+    fun getProjectDetails(@Path("user") user: String, @Path("reponame") peoject: String): Call<Project>
+
+    companion object {
+        val GITHUB_API_URL = "https://api.github.com"
+    }
+}
